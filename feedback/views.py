4006f9e7f7eb7e385f2a9226299a49d2a8a98da4
Minhaj Ap <minhajap00@gmail.com>
2026-05-21 12:03:26 +0530

    Implement Feedback serializer and views for list, create, retrieve, update, and delete operations with user permissions

--- feedback/views.py
+++ feedback/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Feedback
+from .serializers import FeedbackSerializer
+
+
+class FeedbackListCreateView(generics.ListCreateAPIView):
+    
+    serializer_class = FeedbackSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Feedback.objects.all().order_by('-created_at')
+        
+        return Feedback.objects.filter(user = self.request.user).order_by('-created_at')
+    
+    def perform_create(self, serializer):
+        serializer.save(user = self.request.user)
+        
+class FeedbackDetailView(generics.RetrieveUpdateDestroyAPIView):
+    
+    serializer_class = FeedbackSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Feedback.objects.all()
+        return Feedback.objects.filter(user = self.request.user)
+    
+    def perform_update(self, serializer):
+        if not self.request.user.is_staff:
+            serializer.save(status='pending')
+        else:
+            serializer.save()
