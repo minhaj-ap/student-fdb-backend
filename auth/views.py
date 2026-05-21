d0c56c86f6a7aa978dae03eb85b0ed6e2bdc7711
Minhaj Ap <minhajap00@gmail.com>
2026-05-21 12:15:25 +0530

    Implement AutoLoginView for user registration and token generation

---
+++ auth/views.py
@@ -0,0 +1,38 @@
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
+    
+    
+class AutoLoginView(APIView):
+    def post(self, request):
+        
+        username = request.data.get('username')
+        password = request.data.get('password')
+        
+        if not username or not password:
+            return Response({'error': 'Username and password are required.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        user = User.objects.filter(username=username).first()
+        
+        if not user:
+            
+            user = User.objects.create_user(username=username, password=password)
+            
+            authenticate_user = authenticate(username=username, password=password)
+            
+            if not authenticate_user:
+                return Response({'error': 'Authentication failed after user creation.'}, status=status.HTTP_400_BAD_REQUEST)
+            
+            tokens = get_tokens_for_user(authenticate_user)
+            return Response(tokens, status=status.HTTP_200_OK)
+    
--- config/urls.py
+++ config/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from django.urls import path,include
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
 )
+
+from auth.views import AutoLoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/', AutoLoginView.as_view()),
     path('api/token/refresh/', TokenRefreshView.as_view()),
     path('api/register/', include('users.urls')),
     
